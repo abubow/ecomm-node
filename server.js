11f59869fea8e3b78ce94b1180ac0f7486563007
Arshdeep Singh Sandhu <asandhu56@mySeneca.ca>
2023-05-25 16:11:27 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var HTTP_PORT = process.env.PORT || 8080;
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.redirect("/About"); 
+  res.send(
+    "Express http server listening on 8080"
+  );
+});
+
+app.use(express.static('public')); 
+
+// setup http server to listen on HTTP_PORT
+app.listen(process.env.PORT || 8080);
