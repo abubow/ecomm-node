2883b7d4aceb77f5d6f4b5201ce18e8cd44017c7
Arshdeep Singh Sandhu <asandhu56@mySeneca.ca>
2023-05-31 19:17:47 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var HTTP_PORT = process.env.PORT || 8080;
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Route: "/"
-app.get('/', (req, res) => {
-  res.redirect('/about');
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+  res.redirect("/about");
+  res.send("Express http server listening on 8080");
 });
 
-// Route: "/about"
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
-});
+app.use(express.static("public"));
 
-// Start the server
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Express http server listening on port ${port}`);
-});
-
-
-// var express = require("express");
-// var app = express();
-// var HTTP_PORT = process.env.PORT || 8080;
-
-// // setup a 'route' to listen on the default url path
-// app.get("/", (req, res) => {
-//     res.redirect("/about"); 
-//   res.send(
-//     "Express http server listening on 8080"
-//   );
-// });
-
-// app.use(express.static('public')); 
-
-// // setup http server to listen on HTTP_PORT
-// app.listen(process.env.PORT || 8080);
+// setup http server to listen on HTTP_PORT
+app.listen(process.env.PORT || 8080);
