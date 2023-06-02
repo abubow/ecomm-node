d2f5d983eaa5c82466b220fe26ee39248fcec50c
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-06-03 02:29:57 +0500

    formatted and fixed minor error

--- server.js
+++ server.js
@@ -10,66 +10,66 @@ app.use(express.static("public"));
 
 // Setup a route to redirect the root path to "/about"
 app.get("/", (req, res) => {
-  res.redirect("/about");
+	res.redirect("/about");
 });
 
 // Setup a route to return the about.html file from the views folder
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+	res.sendFile(__dirname + "/views/about.html");
 });
 
 // Setup a route to get all items
 app.get("/items", (req, res) => {
-  storeService
-    .getAllItems()
-    .then((items) => {
-      res.json(items);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error });
-    });
+	storeService
+		.getAllItems()
+		.then((items) => {
+			res.json(items);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: error });
+		});
 });
 
 // Setup a route to get all categories
 app.get("/categories", (req, res) => {
-  storeService
-    .getAllCategories()
-    .then((categories) => {
-      res.json(categories);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error });
-    });
+	storeService
+		.getAllCategories()
+		.then((categories) => {
+			res.json(categories);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: error });
+		});
 });
 
 // Setup a route to get published items
 app.get("/shop", (req, res) => {
-  console.log("GET /shop");
-  storeService
-    .getPublishedItems()
-    .then((publishedItems) => {
-      res.json(publishedItems);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error });
-    });
+	console.log("GET /shop");
+	storeService
+		.getPublishedItems()
+		.then((publishedItems) => {
+			res.json(publishedItems);
+		})
+		.catch((error) => {
+			res.status(500).json({ error: error });
+		});
 });
 
 // Setup a 404 route
 app.use((req, res) => {
-  res.status(404).send("Page Not Found");
+	res.status(404).send("Page Not Found");
 });
 
 // Initialize the store-service module
 storeService
-  .initialize()
-  .then(() => {
-    // Start the server only after successful initialization
-    app.listen(HTTP_PORT, () => {
-      console.log("Express http server listening on port " + HTTP_PORT);
-    });
-  })
-  .catch((error) => {
-    // Log the error message if initialization fails
-    console.error("Error initializing store-service:", error);
-  });
+	.initialize()
+	.then(() => {
+		// Start the server only after successful initialization
+		app.listen(HTTP_PORT, () => {
+			console.log("Express http server listening on port " + HTTP_PORT);
+		});
+	})
+	.catch((error) => {
+		// Log the error message if initialization fails
+		console.error("Error initializing store-service:", error);
+	});
