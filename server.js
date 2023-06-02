05356bc8bce52098686d113a447451976b141500
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-06-03 02:09:31 +0500

    updated server.js to serve about.html

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require("express");
 var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
 
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
   res.redirect("/about");
 });
 
-app.use(express.static("public"));
+// setup a route to return the about.html file from the views folder
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, () => {
