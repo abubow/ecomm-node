e55f07cace65d07d75b142ae3a44698b75e6a345
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-06-03 02:07:43 +0500

    fixed minor bug

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var HTTP_PORT = process.env.PORT || 8080;
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
   res.redirect("/about");
-  res.send("Express http server listening on 8080");
 });
 
 app.use(express.static("public"));
 
 // setup http server to listen on HTTP_PORT
-app.listen(process.env.PORT || 8080);
+app.listen(HTTP_PORT, () => {
+  console.log("Express http server listening on port " + HTTP_PORT);
+});
